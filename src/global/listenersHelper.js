e4005e46402af8361ee17e016915aeb85a4405ea
Dmitriy Shishkin <dshishkin@allmax.team>
2018-12-29 12:21:38 +0700

    Import isIos

--- src/global/listenersHelper.js
+++ src/global/listenersHelper.js
@@ -2,16 +2,20 @@
 
 import { Keyboard } from 'react-native';
 
+import {
+  isIos,
+} from './constants';
+
 import type {
   Listeners,
   KeyboardEventListener,
-} from 'types';
+} from '~/types';
 
 export class ListenersManager {
   listeners: Listeners = [];
 
   addKeyboardListeners = ({
-    onHideEventName = isIOS ? 'keyboardWillHide' : 'keyboardDidHide',
+    onHideEventName = isIos ? 'keyboardWillHide' : 'keyboardDidHide',
     onShow,
     onHide,
     otherListeners,
@@ -23,7 +27,7 @@ export class ListenersManager {
   }) => {
     const { listeners } = this;
     listeners.push(
-      Keyboard.addListener(isIOS ? 'keyboardWillShow' : 'keyboardDidShow', onShow),
+      Keyboard.addListener(isIos ? 'keyboardWillShow' : 'keyboardDidShow', onShow),
       Keyboard.addListener(onHideEventName, onHide),
     );
     if (otherListeners != null) {
